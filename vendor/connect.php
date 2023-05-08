94fda4952299481a7545e53fd0acf097f4b8f2f4
f3ntd3v <98050622+fentdevsex@users.noreply.github.com>
2023-05-09 02:02:03 +0300

    Update connect.php

--- vendor/connect.php
+++ vendor/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('f0794045.xsph.ru', 'f0794045_systemrevo', 'systemrevo', 'f0794045_systemrevo');
+$connect = mysqli_connect('', '', '', '');
 
     if(!$connect){
         die('Error connect to database');
